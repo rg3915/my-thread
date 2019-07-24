6da0d93f0aca7780bd095cb6afad612c4ee34fde
rg3915 <rg3915@yahoo.com.br>
2019-07-24 02:50:05 -0300

    Callback and kwargs.

--- myproject/core/my_thread.py
+++ myproject/core/my_thread.py
@@ -4,26 +4,27 @@ import threading
 
 class MyThread(threading.Thread):
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, * args, **kwargs):
         threading.Thread.__init__(self)
-        print('__init__')
+        self.result = 0
 
     def run(self):
         print('Start run...')
         self.my_process()
         print('End run...')
         # requests.get('http://localhost:8000/person/')
-        print('Chamar my_process2...')
-        self.my_process2()
 
     def my_process(self):
         print('Sart my_process.')
         for i in range(11):
             print(i)
+        self.result = i
         print('End my_process.')
+        context = {'result': self.result}
+        self.cb(self, **context)
 
-    def my_process2(self):
-        print('Start my_process2.')
-        for i in range(11):
-            print(i * 2)
-        print('End my_process2.')
+    def cb(self, *args, **kwargs):
+        '''
+        Callback
+        '''
+        print('Callback!', 'result:', kwargs['result'])
